211f4364de7f5244ca39e7be82998f19d60f7bea
murzum <murzum@protonmail.com>
2019-07-21 14:24:14 +0300

    Valmis - Osa1 - a

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -2,27 +2,48 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const pageHeader = 'Kurssitiedot'
+  const part_1_title = 'boo'
+  const part_2_title = 'boo2'
+  const part_3_title = 'boo3'
+  const part_1_count = 1
+  const part_2_count = 2
+  const part_3_count = 3
+
+  const Header = (props) => {
+    return (
+        <h1>{props.pageHeader}</h1>
+    )
+  }
+
+  const ContentPart = (props) => {
+    return (
+        <p><h3>Kurssin osa: {props.name}, Tehtävien lkm: {props.count}</h3></p>
+    )    
+  }
+
+  const Content = (props) => {
+    return (
+      <div>
+        <ContentPart name = {part_1_title} count = {part_1_count} />
+        <ContentPart name = {part_2_title} count = {part_2_count} />
+        <ContentPart name = {part_3_title} count = {part_3_count} />
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+      <div><p>Tehtävien lkm yhteensä: {part_1_count + part_2_count + part_3_count}</p></div>
+    )
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header pageHeader = {pageHeader} />
+      <Content />
+      <Total />
     </div>
   )
 }
