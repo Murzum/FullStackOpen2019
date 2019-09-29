e2a18633264c647153b9d272ac17e67194e29b77
murzum <murzum@protonmail.com>
2019-09-29 19:07:04 +0300

    osa2 - puhelinluettelo - Step4 -valmis

--- osa2/puhelinluettelo/src/components/App.js
+++ osa2/puhelinluettelo/src/components/App.js
@@ -2,11 +2,33 @@ import React, { useState } from 'react'
 import Person from './Person.js'
 
 const App = () => {
-    const [ persons, setPersons ] = useState([]) 
+    const [ persons, setPersons ] = useState([
+      { id: 'Arto Hellas', name: 'Arto Hellas', number: '040-123456' },
+      { id: 'Ada Lovelace', name: 'Ada Lovelace', number: '39-44-5323523' },
+      { id: 'Dan Abramov', name: 'Dan Abramov', number: '12-43-234345' },
+      { id: 'Mary Poppendieck', name: 'Mary Poppendieck', number: '39-23-6423122' }      
+    ]) 
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
+    const [ newFilter, setNewFilter ] = useState('')
+    
+    const handleNameChange = (event) => {
+      setNewName(event.target.value)  
+    }
+    const handleNumberChange = (event) => {
+      setNewNumber(event.target.value)  
+    }
+    const handleFilterChange = (event) => {
+      setNewFilter(event.target.value)
+    }
 
-    const rows = () => persons.map(person =>
+    const existsInArray = (props) => {
+      return person => person.name.includes(props.newFilter)
+    }
+
+    const filteredNumbers = persons.filter(existsInArray({newFilter}))
+    
+    const rows = () => filteredNumbers.map(person =>
       <Person
         key={person.id}
         name={person.name}
@@ -27,25 +49,20 @@ const App = () => {
       }
 
       if (persons.some(checkName)) {
-        window.alert(`Nimi ${newName} on jo listassa. Ei voi lisätä!`)
+        window.alert(`Name ${newName} already exists. Name not added!`)
       }
       else {
         setPersons(persons.concat(rowObject))
         setNewName('')
         setNewNumber('')
       }
-
     }   
-    const handleNameChange = (event) => {
-      setNewName(event.target.value)  
-    }
-    const handleNumberChange = (event) => {
-      setNewNumber(event.target.value)  
-    }
 
     return (
       <div>
         <h2>Phonebook</h2>
+        <div>Filter phonebook using: <input value={newFilter} onChange={handleFilterChange}/></div>
+        <div><h3>Add new:</h3></div>
         <form onSubmit={handleSubmit}>
           <div>Name: <input value={newName} onChange={handleNameChange}/></div>
           <div>Number: <input value={newNumber} onChange={handleNumberChange}/></div>
